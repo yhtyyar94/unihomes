dff382286c7dbeda3e4f6e34aeaaa8d2a9cc66cf
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-05 20:21:52 +0000

    ....

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacthhh
-        </a>
-        <h1>talha</h1>
-
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -33,13 +15,9 @@ function App() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Learn React
+					Learn Reacthhh
 				</a>
-				<h2>Elif burada.</h2>
-				<h2>Mursel is here</h2>
-				<h2>aaaa</h2>
-				<h1>New Project</h1>
-
+				<h1>talha</h1>
 			</header>
 		</div>
 	);
