0a50c3f22d90bbe8b516a6e33ed62c43efdcd9b8
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-05 20:01:18 +0000

    modified return

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,33 +17,11 @@ function App() {
 				>
 					Learn React
 				</a>
-<<<<<<< HEAD
-
 				<h2>Elif burada.</h2>
-
 				<h2>Mursel is here</h2>
-
 				<h2>aaaa</h2>
-=======
 				<h1>New Project</h1>
-			</header>
-		</div>
-	);
-
-
-
-        <h2>aaaa</h2>
-
-
-
-      </header>
-      <h2>Cagri is here</h2>
-    </div>
-  );
-
->>>>>>> aeb90ee3d186da202240e68342ad9e65c5826246
-
-				<h2>Mustafa was here</h2>
+				<h2>aaaaaaaaa</h2>;
 			</header>
 		</div>
 	);
