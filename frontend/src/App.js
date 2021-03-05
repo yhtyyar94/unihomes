25876691b41d6bd855d1ef9112a266abb402c11e
Elif Balci <mrselifbalci@gmail.com>
2021-03-05 20:08:02 +0000

    aa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacthhh
         </a>
 
         <h2>Elif burada.</h2>
