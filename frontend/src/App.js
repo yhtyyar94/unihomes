1c29e968738124d49efc97ddf6b157eae4d4d2fa
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-05 19:58:38 +0000

    modified return

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,40 +17,12 @@ function App() {
 				>
 					Learn React
 				</a>
-				<h1>New Project</h1>
-			</header>
-		</div>
-	);
-
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
 
 				<h2>Elif burada.</h2>
 
-
 				<h2>Mursel is here</h2>
 
-        <h2>aaaa</h2>
-
-
-
-      </header>
-    </div>
-  );
-
+				<h2>aaaa</h2>
 
 				<h2>Mustafa was here</h2>
 			</header>
