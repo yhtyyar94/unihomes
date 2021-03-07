f55a9568fc12c9b53350619c2fa3db56e5f98eba
blackeagle94 <70913322+blackeagle94@users.noreply.github.com>
2021-03-07 16:10:24 +0200

    updated comment line

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-/**Create .env file in backend folder and your mongodb connection as db_connection**/
+/**Create .env file in backend folder and add your mongodb connection as db_connection**/
 
 module.exports = () => {
     mongoose.connect(process.env.db_connection, {useNewUrlParser:true, useUnifiedTopology:true})
