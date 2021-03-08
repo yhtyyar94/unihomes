22b5598a51100cf0a33b7810cf0a898f2b77f6e1
Dilekcn <canturkdk@gmail.com>
2021-03-08 20:47:56 +0000

    new one

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+// Hello from new user
 //db connection
 require('./config/db.config')();
 
