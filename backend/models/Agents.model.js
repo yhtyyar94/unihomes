a99598d64e9c65d8d36c14c6f1ce56bb5427a561
Dilekcn <canturkdk@gmail.com>
2021-03-10 15:13:17 +0000

    added letting agent

--- backend/models/Agents.model.js
+++ backend/models/Agents.model.js
@@ -7,14 +7,30 @@ const UserSchema = new Schema({
         type: String,
         require: [true, 'Please enter an email'],
         unique: [true, 'This email is exist. Please enter new email or try to login.'],
-        lovercase:true,
+        lowercase:true,
         validate: [isEmail, 'Please enter a valid email.']
     },
     password: {
         type: String,
         require:[true, 'Please enter a password'],
         minLength:[6, 'Minimum password length is 6 characters.']
-    }
+    },
+    name:{
+        type: String,
+        require: [true, 'Please enter your name']
+    },
+    contactNumber:{
+        type: Number,
+        require: [true, 'Please enter your phone number']
+    },
+    company:{
+        type: String
+   },
+   city:{
+    type: String,
+    require: [true, 'Please choose a city ']
+   }
+
 })
 
 module.exports = mongoose.model('user', UserSchema)
