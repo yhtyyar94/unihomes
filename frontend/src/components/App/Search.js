fe75de8f7d1377d5e96a134632b67c9473a79080
Elif Balci <mrselifbalci@gmail.com>
2021-03-11 22:24:53 +0000

    added the image and overlay on homepage

--- frontend/src/components/App/Search.js
+++ frontend/src/components/App/Search.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import view from './images/view.png'
+import './App.css'
 
 export default function Search() {
     return (
+     
         <div>
-          
+            <div className="container">
+                <img src={view} alt="view" className="img"/>
+                <div className="titleText">Find student homes with bills included</div>
+                <div className="subText">A simple and faster way to search for student accommodation</div>
+                <div className="overlay"></div>
+            </div>
         </div>
     )
 }
