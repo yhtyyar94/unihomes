5ab3f37e4af34214a7b6364b59ed338ddc880ad5
Elif Balci <mrselifbalci@gmail.com>
2021-03-11 21:06:04 +0000

    updated indej.js

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from './Search'
+
+
+export default function App() {
+    return (
+        <div>
+            <Search />
+            
+        </div>
+    )
+}
