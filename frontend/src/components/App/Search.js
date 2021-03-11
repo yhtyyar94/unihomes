faf60efc2dcf827e7c7aaefdb35c40b8a2e6a8fb
Elif Balci <mrselifbalci@gmail.com>
2021-03-11 23:03:14 +0000

    input area created

--- frontend/src/components/App/Search.js
+++ frontend/src/components/App/Search.js
@@ -12,6 +12,13 @@ export default function Search() {
                 <div className="subText">A simple and faster way to search for student accommodation</div>
                 <div className="overlay"></div>
             </div>
+            <div className="search-container">
+                <form className="form">
+                    <input type="text" className="input-one" placeHolder="Search by city"/>
+                    <input className="input-two" placeHolder="Any bedrooms"/>
+                    <button className="btn" >Find Homes</button>
+                </form>
+            </div>
         </div>
     )
 }
