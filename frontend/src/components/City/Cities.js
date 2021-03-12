b10edc8e622665fb25e2dba91400e1dcc1bdf77e
Elif Balci <mrselifbalci@gmail.com>
2021-03-12 18:25:55 +0000

    city page updated

--- frontend/src/components/City/Cities.js
+++ frontend/src/components/City/Cities.js
@@ -41,7 +41,7 @@ const [homes,setHomes]=useState(data)
                     <option>option 3</option>
                 </select>
             </form>
-        </div>
+        </div> 
         <div >
                 <p> {homes.filter(item=>item.city==='Liverpool').length} homes in {homes[0].city}</p>
                 <div className="homes">
@@ -49,13 +49,16 @@ const [homes,setHomes]=useState(data)
                     <City home={home}/>       
                     )}
                 </div>
+                {/* <button>View More</button> */}
        </div>
+       
        <div className="banner-bottom">
            <div className="text">
                     <h1 >Being a student in Liverpool</h1>
                     <h5 >An abundance of shops, bars, restaurants, and nightclubs have collectively put Liverpool well and truly on the student map. The city is known for being a party destination (made famous by Geordie Shore), and there’s plenty of this on offer for students. With a diverse mix of places to eat, drink and party, there's no wonder it's such a popular city to study in.</h5>
             </div>                      
         </div>
+       
             
     </div>
     )
