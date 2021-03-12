a8d2c699f4a586e190feeeba1f0ecd7202c5fe1a
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-12 16:52:49 +0000

    .

--- frontend/src/components/City/Data.js
+++ frontend/src/components/City/Data.js
@@ -1,133 +1,136 @@
 const homes = [
-            {  
-                id:1,
-                city:'Liverpool',
-                image:'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-                rent: 106,
-                bedroom:4,
-                bathroom:2,
-                address:{
-                    street:'Holmes',
-                    district:'Kentish Town',
-                    postcode:'NW5 3AN'
-                },
-                type:'House',
-                keyFeatures:[
-                    '8 bedroom house in Highfield within easy access of the university and the local amenities',
-                    'Central heating',
-                    'Double glazing',
-                    'Parking available',
-                    'A few minutes walk from University of Southampton Highfield Campus'
-                ]
-            },
-            {
-                id:2,
-                city:'Liverpool',
-                image:'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-                rent: 106,
-                bedroom:4,
-                bathroom:2,
-                address:{
-                    street:'Holmes',
-                    district:'Kentish Town',
-                    postcode:'NW5 3AN'
-                },
-                type:'House',
-                keyFeatures:[
-                    '8 bedroom house in Highfield within easy access of the university and the local amenities',
-                    'Central heating',
-                    'Double glazing',
-                    'Parking available',
-                    'A few minutes walk from University of Southampton Highfield Campus'
-                ]
-            },
-            {
-                id:3,
-                city:'Liverpool',
-                image:'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-                rent: 106,
-                bedroom:4,
-                bathroom:2,
-                address:{
-                    street:'Holmes',
-                    district:'Kentish Town',
-                    postcode:'NW5 3AN'
-                },
-                type:'House',
-                keyFeatures:[
-                    '8 bedroom house in Highfield within easy access of the university and the local amenities',
-                    'Central heating',
-                    'Double glazing',
-                    'Parking available',
-                    'A few minutes walk from University of Southampton Highfield Campus'
-                ]
-            },
-            {  
-                id:4,
-                city:'Birmingham',
-                image:'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-                rent: 106,
-                bedroom:4,
-                bathroom:2,
-                address:{
-                    street:'Holmes',
-                    district:'Kentish Town',
-                    postcode:'NW5 3AN'
-                },
-                type:'House',
-                keyFeatures:[
-                    '8 bedroom house in Highfield within easy access of the university and the local amenities',
-                    'Central heating',
-                    'Double glazing',
-                    'Parking available',
-                    'A few minutes walk from University of Southampton Highfield Campus'
-                ]
-            },
-            {
-                id:5,
-                city:'Birmingham',
-                image:'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-                rent: 106,
-                bedroom:4,
-                bathroom:2,
-                address:{
-                    street:'Holmes',
-                    district:'Kentish Town',
-                    postcode:'NW5 3AN'
-                },
-                type:'House',
-                keyFeatures:[
-                    '8 bedroom house in Highfield within easy access of the university and the local amenities',
-                    'Central heating',
-                    'Double glazing',
-                    'Parking available',
-                    'A few minutes walk from University of Southampton Highfield Campus'
-                ]
-            },
-            {
-                id:6,
-                city:'Birmingham',
-                image:'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
-                rent: 106,
-                bedroom:4,
-                bathroom:2,
-                address:{
-                    street:'Holmes',
-                    district:'Kentish Town',
-                    postcode:'NW5 3AN'
-                },
-                type:'House',
-                keyFeatures:[
-                    '8 bedroom house in Highfield within easy access of the university and the local amenities',
-                    'Central heating',
-                    'Double glazing',
-                    'Parking available',
-                    'A few minutes walk from University of Southampton Highfield Campus'
-                ]
-            }
-    
-        
-]
-
+	{
+		id: 1,
+		city: 'Liverpool',
+		image:
+			'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+		rent: 106,
+		bedroom: 4,
+		bathroom: 2,
+		address: {
+			street: 'Holmes',
+			district: 'Kentish Town',
+			postcode: 'NW5 3AN',
+		},
+		type: 'House',
+		keyFeatures: [
+			'8 bedroom house in Highfield within easy access of the university and the local amenities',
+			'Central heating',
+			'Double glazing',
+			'Parking available',
+			'A few minutes walk from University of Southampton Highfield Campus',
+		],
+	},
+	{
+		id: 2,
+		city: 'Liverpool',
+		image:
+			'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+		rent: 106,
+		bedroom: 4,
+		bathroom: 2,
+		address: {
+			street: 'Holmes',
+			district: 'Kentish Town',
+			postcode: 'NW5 3AN',
+		},
+		type: 'House',
+		keyFeatures: [
+			'8 bedroom house in Highfield within easy access of the university and the local amenities',
+			'Central heating',
+			'Double glazing',
+			'Parking available',
+			'A few minutes walk from University of Southampton Highfield Campus',
+		],
+	},
+	{
+		id: 3,
+		city: 'Liverpool',
+		image:
+			'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+		rent: 106,
+		bedroom: 4,
+		bathroom: 2,
+		address: {
+			street: 'Holmes',
+			district: 'Kentish Town',
+			postcode: 'NW5 3AN',
+		},
+		type: 'House',
+		keyFeatures: [
+			'8 bedroom house in Highfield within easy access of the university and the local amenities',
+			'Central heating',
+			'Double glazing',
+			'Parking available',
+			'A few minutes walk from University of Southampton Highfield Campus',
+		],
+	},
+	{
+		id: 4,
+		city: 'Birmingham',
+		image:
+			'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+		rent: 106,
+		bedroom: 4,
+		bathroom: 2,
+		address: {
+			street: 'Holmes',
+			district: 'Kentish Town',
+			postcode: 'NW5 3AN',
+		},
+		type: 'House',
+		keyFeatures: [
+			'8 bedroom house in Highfield within easy access of the university and the local amenities',
+			'Central heating',
+			'Double glazing',
+			'Parking available',
+			'A few minutes walk from University of Southampton Highfield Campus',
+		],
+	},
+	{
+		id: 5,
+		city: 'Birmingham',
+		image:
+			'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+		rent: 106,
+		bedroom: 4,
+		bathroom: 2,
+		address: {
+			street: 'Holmes',
+			district: 'Kentish Town',
+			postcode: 'NW5 3AN',
+		},
+		type: 'House',
+		keyFeatures: [
+			'8 bedroom house in Highfield within easy access of the university and the local amenities',
+			'Central heating',
+			'Double glazing',
+			'Parking available',
+			'A few minutes walk from University of Southampton Highfield Campus',
+		],
+	},
+	{
+		id: 6,
+		city: 'Birmingham',
+		image:
+			'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY',
+		rent: 106,
+		bedroom: 4,
+		bathroom: 2,
+		address: {
+			street: 'Holmes',
+			district: 'Kentish Town',
+			postcode: 'NW5 3AN',
+		},
+		type: 'House',
+		keyFeatures: [
+			'8 bedroom house in Highfield within easy access of the university and the local amenities',
+			'Central heating',
+			'Double glazing',
+			'Parking available',
+			'A few minutes walk from University of Southampton Highfield Campus',
+		],
+	},
+];
 
 export default homes;
