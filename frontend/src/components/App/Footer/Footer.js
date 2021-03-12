8382de17c20c9a2968a014624670df35f5122a66
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-12 18:24:22 +0000

    added footer nav

--- frontend/src/components/App/Footer/Footer.js
+++ frontend/src/components/App/Footer/Footer.js
@@ -4,14 +4,34 @@ import './Footer.css';
 const Footer = () => {
 	return (
 		<div>
+			<div style={{ height: 500 }}></div>
 			<div className="keepintouch-sm">
 				<h1 id="keepintouch">Keep In Touch</h1>
-				<form>
+				<form id="form-keepintouch">
 					<input
 						id="input-keepintouch"
 						placeholder="Enter your email address..."
 					/>
+					<button id="btn-keepintouch" type="submit">
+						Click
+					</button>
 				</form>
+			</div>
+			<div id="nav-footer">
+				<nav>
+					<ul>
+						<li>
+							<a href="#aboutus">About Us</a>
+						</li>
+						<li>
+							<a href="#terms">Terms & Conditions</a>
+						</li>
+						<li>
+							<a href="#policies">Privacy & Cookie Policies</a>
+						</li>
+						<li>2021 &copy; SoftwareChasers</li>
+					</ul>
+				</nav>
 			</div>
 		</div>
 	);
