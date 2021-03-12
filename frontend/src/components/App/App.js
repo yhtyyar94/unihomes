ecb94b4387dd639aed3fe93414c8db04ca63e0f0
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-12 16:46:18 +0000

    added footer upper div

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import Search from './Search/Search'
+import React from 'react';
+import Search from './Search/Search';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import City from '../City/City';
 import Header from './Header/Header';
-
+import Footer from './Footer/Footer';
 
 export default function App() {
-    return (
-        <div>
-            <Header />
-           
-           
-            <Router>
-                    <Switch>
-                        <Route exact path="/" component={Search} />
-                        <Route exact path="/city" component={City} />
-                    </Switch>
-            </Router>
-                
-            
-        </div>
-    )
+	return (
+		<div>
+			<Header />
+
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Search} />
+					<Route exact path="/city" component={City} />
+				</Switch>
+			</Router>
+			<Footer />
+		</div>
+	);
 }
