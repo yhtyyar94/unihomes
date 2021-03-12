d048e127d8d9c6f342eca052ae9b8c4b607234b4
Ahmet Çağrı <cagricavdar01@gmail.com>
2021-03-12 18:40:26 +0100

    Header.js updated

--- frontend/src/components/App/Header/Header.js
+++ frontend/src/components/App/Header/Header.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div style={{background:'pink',height:60,width:'100%'}}>
-            Header
-        </div>
+        <div style={{background:'#03C5F0',height:100,width:'100%'}}>
+        <p style={{color: 'white',}}> Unihomes </p>
+        <div className="block1" style={{textAlign:'right', color: 'white',}}>
+Shortlist Login Search Contact
+</div>
+
+     </div>
     )
 }
