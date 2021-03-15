9e2018d0014e79cab61aac90810fe0d4d4c1051d
Ahmet Çağrı <cagricavdar01@gmail.com>
2021-03-15 12:16:54 +0100

    header_cagri

--- frontend/src/components/App/Header/Header.js
+++ frontend/src/components/App/Header/Header.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
+import { MdHome } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
+import { MdPerson } from "react-icons/md";
+import { MdMail } from "react-icons/md";
+import { MdBookmark } from "react-icons/md";
+
+
+
+
 
 export default function Header() {
     return (
-        <div style={{background:'#03C5F0',height:100,width:'100%'}}>
-        <p style={{color: 'white',}}> Unihomes </p>
-        <div className="block1" style={{textAlign:'right', color: 'white',}}>
-Shortlist Login Search Contact
-</div>
+        <div id="header">
+        
+        
+        <div className = "unihomes-h">
+            <ul>
+                
+            </ul>
+            <section className="headernavbar" style={{height: 75, fontSize: 17}}>
+                <li >
+                <h2 a href="/" id="unihomes" style={{fontSize: 35}}><MdHome />Unihomes</h2>
+                </li>
+                <a href ="/LoginPop" className="navbar-item"><MdPerson /> Login</a>               
+                <a href ="/shortlist" className="navbar-item"><MdBookmark /> Shortlist</a>
+              <a href ="/contact" className="navbar-item"><MdMail /> Contant Us</a> 
+               <a href ="/" className="navbar-item"><MdSearch /> Search</a>
+      
+            </section>
+        </div>
 
-     </div>
+        </div>
+
+     
     )
 }
