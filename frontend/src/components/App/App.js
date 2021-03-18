bcbe7ba4d0ba2400d94e116feb4f6e3df22bf92a
blackeagle94 <blackeagle4894@gmail.com>
2021-03-18 14:55:12 +0200

    ismail

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios';
+import React,{useState,useEffect,useRef} from 'react';
+import axios from 'axios'
 import Search from './Search/Search';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Cities from '../City/Cities';
@@ -11,94 +11,60 @@ import Shortlist from './Shortlist';
 import About from '../StaticPages/About';
 import Terms from '../StaticPages/Terms';
 import Policies from '../StaticPages/Policies';
-import LoginPop from '../App/Header/LoginPop';
-import RegisterPop from '../App/Header/RegisterPop';
 
 export default function App() {
-	const [cities, setCities] = useState([]);
-	const [homes, setHomes] = useState([]);
-	// const [currentCity, setCurrentCity] = useState('Liverpool');
-	// const [roomCount, setRoomCount] = useState(4);
-	const [login, setLog] = useState(false);
-	const [signup, setSignUp] = useState();
-
-	useEffect(() => { 
-		axios
-			.get('http://localhost:5000/cities')
-			.then((res) => {
-				setCities(res.data);
-			}) 
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
-
-	useEffect(() => {
-		axios
-			.get('http://localhost:5000/homes')
-			.then((res) => {
-				setHomes(res.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
 
 
-
+    const[cities,setCities]=useState([])
+	const[homes,setHomes]=useState([])
+	const[currentCity,setCurrentCity]=useState('Liverpool')
+	const[roomCount,setRoomCount]=useState(4)
 	
 
-	const toggleLogin = () => {
-		setLog(!login);
-	};
-	const register = () => {
-		setLog(false);
-		setSignUp(true);
-	};
-	const backtoLogin = () => {
-		setSignUp(false);
-		setLog(true);
-	};
 
+    useEffect(()=>{
+        axios.get('http://localhost:5000/cities')
+        .then(res=>{ 
+            setCities(res.data)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[])
+
+	useEffect(()=>{
+        axios.get('http://localhost:5000/homes')
+        .then(res=>{
+            setHomes(res.data)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[])
+
+	 const handleSubmit =(cityName,roomNum)=>{
+		 setCurrentCity(cityName)
+		 setRoomCount(roomNum)
+	 }
+	 const filterBedrooms=(bedroom)=>{
+		let filteredHomes=homes.filter(home=>home.bedroom===bedroom)
+		setHomes(filteredHomes)
+	
+	 }
+    
 	
 	return (
 		<div>
-			<Header toggleLogin={toggleLogin} />
-			{login === true ? (
-				<div
-					style={{ position: 'absolute', marginLeft: '300px', zIndex: 30 }}
-				>
-					<LoginPop register={register} />
-				</div>
-			) : null}
-			{signup === true ? (
-				<div
-					style={{ position: 'absolute', marginLeft: '300px', zIndex: 30 }}
-				>
-					<RegisterPop register={register} backtoLogin={backtoLogin} />
-				</div>
-			) : null} 
+			<Header />
 			<Router>
 				<Switch>
-					<Route
-						exact
-						path="/"
-						render={() => (
-							<Search
-								cities={cities}
-							/>
-						)}
-					/>
-					<Route
-						
-						path={`/cities/:id/:bedroom`}
-						render={() => (
-							<Cities
-								homes={homes}
-								cities={cities}
-							/>
-						)}
-					/>
+					<Route exact path="/" render={()=><Search cities={cities} currentCity={currentCity} 
+					handleSubmit={handleSubmit} roomCount={roomCount}/>} />
+					<Route exact path="/city" render={()=><Cities homes={homes} 
+					currentCity={currentCity} 
+					roomCount={roomCount}
+					filterBedrooms={filterBedrooms}
+					/>} />
 					<Route exact path="/homedetails" component={HomeDetails} />
 					<Route exact path="/shortlists" component={Shortlist} />
 					<Route exact path="/aboutus" component={About} />
@@ -110,4 +76,3 @@ export default function App() {
 		</div>
 	);
 }
- 
