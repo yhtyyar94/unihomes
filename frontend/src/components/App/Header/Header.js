89dc393a098dce2e9a59a11656ca209df6fb2189
blackeagle94 <blackeagle4894@gmail.com>
2021-03-22 19:59:10 +0200

    repaired header opacity problem

--- frontend/src/components/App/Header/Header.js
+++ frontend/src/components/App/Header/Header.js
@@ -9,9 +9,12 @@ import { GoHeart } from 'react-icons/go';
 import { MdBookmark } from 'react-icons/md';
 import { Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap';
 import Login from './LoginPop.js';
+import { useHistory} from 'react-router-dom'
 
 export default function Header({ toggleLogin }) {
 	const [visible, setVisibility] = useState(true)
+	const [history, setHistory] = useState('/')
+	const pathHistory = useHistory()
 	const changeClass = () => {
 		if(window.location.pathname === '/' || window.location.pathname === '/student-accommodation') {
 			if (window.pageYOffset > 0) {
@@ -26,11 +29,21 @@ export default function Header({ toggleLogin }) {
 		visibility: visible ? 'hidden' : 'visible'
 	}
 
+	const changeUrl = () => {
+		if(history !== window.location.pathname){
+			setHistory(window.location.pathname)
+		}
+	}
+
 	useEffect(() => {
 		window.addEventListener("scroll", changeClass)
+		window.addEventListener("click", changeUrl)	
+		changeUrl()
 	},[])
+
+	console.log(history);
 	return (
-		<div className={window.location.pathname === '/' || window.location.pathname === '/student-accommodation' ? 'header' : 'scroll'} id="header">
+		<div className={history === '/' || history === '/student-accommodation' ? 'header' : 'scroll'} id="header">
 			<div className="header-logo">
 				<a href="/" id="unihomes" style={{ fontSize: 35 }}>
 					<MdHome className="home-logo"/>Unihomes
