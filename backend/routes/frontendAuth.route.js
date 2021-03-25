816a7d8997d046021383f2827718d675d70b333b
blackeagle94 <blackeagle4894@gmail.com>
2021-03-26 01:09:13 +0200

    added user auth

--- backend/routes/frontendAuth.route.js
+++ backend/routes/frontendAuth.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     const token = req.params.token || req.body.token || req.headers['x-access-token'] || req.headers.token
 
     if (token) {
