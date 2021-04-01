a6fd4a792a91d8160b4658d1c9bcd8a3efee6929
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-04-01 18:38:38 +0100

    updated footer

--- frontend/src/components/App/Shortlist.js
+++ frontend/src/components/App/Shortlist.js
@@ -1,51 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import {BiEnvelope} from 'react-icons/bi'
-import ShortListModal from '../ShortListBook'
-import City from '../City/City'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import { BiEnvelope } from 'react-icons/bi';
+import ShortListModal from '../ShortListBook';
+import City from '../City/City';
+import axios from 'axios';
 
+const Shortlist = ({ shortlist, setShortlist, changeShortlist }) => {
+	const [shortlistLocal, setShortlistLocal] = useState([]);
+	const [homesShortlist, setHomesShortlist] = useState([]);
 
-const Shortlist = ({shortlist, setShortlist, changeShortlist}) => {
+	useEffect(() => {
+		setShortlistLocal(JSON.parse(localStorage.getItem('shortlist')));
+		console.log(JSON.parse(localStorage.getItem('shortlist')));
+		let array = [];
+		for (
+			let i = 0;
+			i < JSON.parse(localStorage.getItem('shortlist')).length;
+			i++
+		) {
+			axios
+				.get(
+					`http://localhost:5001/api/getproperties/${
+						JSON.parse(localStorage.getItem('shortlist'))[i]
+					}`,
+				)
+				.then((res) => {
+					array.push(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+		setHomesShortlist(array);
+		window.scroll(0, 0);
+	}, []);
 
-    const [shortlistLocal, setShortlistLocal] = useState([])
-    const [homesShortlist, setHomesShortlist] = useState([]);
+	return (
+		<div className="shortlist">
+			<div className="shortlist-header">
+				<h1>Shortlist</h1>
+			</div>
+			<div className="shortlist-counter">
+				<h3>
+					{shortlistLocal ? shortlistLocal.length : 0} property shortlisted
+				</h3>
+			</div>
 
+			<div className="shortlist-body" style={{ backgroundColor: '#e5e5e5' }}>
+				{homesShortlist &&
+					homesShortlist.map((item) => (
+						<City
+							home={item}
+							shortlist={shortlist}
+							setShortlist={setShortlist}
+							changeShortlist={changeShortlist}
+						/>
+					))}
+			</div>
+		</div>
+	);
+};
 
-
-    useEffect(() => {
-        setShortlistLocal(JSON.parse(localStorage.getItem('shortlist')))
-        console.log(JSON.parse(localStorage.getItem('shortlist')))
-        let array =[]
-        for(let i = 0; i < JSON.parse(localStorage.getItem('shortlist')).length; i++) {
-            axios.get(`http://localhost:5001/api/getproperties/${JSON.parse(localStorage.getItem('shortlist'))[i]}`)
-          .then((res) => {
-            array.push(res.data)
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-        }
-        setHomesShortlist(array)
-        window.scroll(0,0)
-      }, []);
-
-    return ( 
-        <div className="shortlist">
-            <div className="shortlist-header">
-                <h1>Shortlist</h1>
-            </div>
-            <div className="shortlist-counter">
-                <h3>{shortlistLocal ? shortlistLocal.length : 0} property shortlisted</h3>
-         
-            </div>
-     
-            <div className="shortlist-body" style={{backgroundColor:"#e5e5e5"}}>
-                {homesShortlist && homesShortlist.map(item => <City home={item} shortlist={shortlist} setShortlist={setShortlist} changeShortlist={changeShortlist}/>)}
-            </div>
-        </div>
-    )
-}
-
-export default Shortlist
-
-
+export default Shortlist;
