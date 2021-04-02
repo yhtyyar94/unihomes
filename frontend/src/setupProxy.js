298ef86aac5f9c53c57889a0670029461cffba63
blackeagle94 <70913322+blackeagle94@users.noreply.github.com>
2021-04-02 17:03:09 +0300

    .

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,4 +2,5 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = app => {
     app.use(proxy("/api/*", { target: "https://unilive-backend.herokuapp.com" }));
+    app.use(proxy("/api/*", { target: "https://unilive.herokuapp.com" }));
   };
