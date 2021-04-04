3ff4d9ca9dfeee73f2a1df4ba6764d912a1dd4ee
blackeagle94 <blackeagle4894@gmail.com>
2021-04-04 13:09:58 +0300

    ismail

--- frontend/src/components/App/Agency/AddProperty/AddProperty.js
+++ frontend/src/components/App/Agency/AddProperty/AddProperty.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
 import './AddProperty.css';
-import Cloudinary from './Cloudinary';
 
 const AddProperty = ({userInfo}) => {
 	const [ cities, setCities ] = useState(null);
