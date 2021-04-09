73797afdfb02005fbd31bbb5b32d4248c62860de
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-04-09 16:23:24 +0100

    updated footer responsiveness

--- frontend/src/components/HomeDetails/HomeDetails.js
+++ frontend/src/components/HomeDetails/HomeDetails.js
@@ -62,7 +62,7 @@ export default function HomeDetails({
 		setShortlist(idList);
 	};
 
-    const removeFromShortlist = () => {
+	const removeFromShortlist = () => {
 		let newShortlist = shortlist;
 
 		for (let i = 0; i < newShortlist.length; i++) {
@@ -100,45 +100,42 @@ export default function HomeDetails({
 
 	return (
 		<div>
-					<Modal
-						isOpen={modalIsOpen}
-						onRequestClose={() => setModalIsOpen(false)}
-						style={{
-							overlay: {
-								top: 35,
-								backgroundColor: 'rgba(211, 211, 211, 0.60)',
-							},
-							content: {
-								padding: 2,
-								height: 700,	
-							},
-						}}
-					>
-						<img
-							src={
-								home.length !== 0 &&
-								home.images[imageIndex]
-							}
-							style={{
-								borderRadius: '3px',
-								height: '100%',
-								width: '100%',
-							}}
-							alt=""
-						/>
-						
-						<BiRightArrow
-							className="arrow-right"
-							onClick={handleRightSwipe}
-							fill="#03c5f0"
-						/>
-						
-						<BiLeftArrow
-							className="arrow-left"
-							onClick={handleLeftSwipe}
-							fill="#03c5f0"
-						/>
-					</Modal>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={() => setModalIsOpen(false)}
+				style={{
+					overlay: {
+						top: 35,
+						backgroundColor: 'rgba(211, 211, 211, 0.60)',
+					},
+					content: {
+						padding: 2,
+						height: 700,
+					},
+				}}
+			>
+				<img
+					src={home.length !== 0 && home.images[imageIndex]}
+					style={{
+						borderRadius: '3px',
+						height: '100%',
+						width: '100%',
+					}}
+					alt=""
+				/>
+
+				<BiRightArrow
+					className="arrow-right"
+					onClick={handleRightSwipe}
+					fill="#03c5f0"
+				/>
+
+				<BiLeftArrow
+					className="arrow-left"
+					onClick={handleLeftSwipe}
+					fill="#03c5f0"
+				/>
+			</Modal>
 			<div className="homedetails-back-to-search">
 				<form>
 					<button
@@ -171,10 +168,7 @@ export default function HomeDetails({
 
 							<img
 								onDoubleClick={() => setModalIsOpen(true)}
-								src={
-									home.length !== 0 &&
-									home.images[imageIndex]
-								}
+								src={home.length !== 0 && home.images[imageIndex]}
 								style={{ borderRadius: '3px' }}
 								alt=""
 							/>
@@ -188,10 +182,7 @@ export default function HomeDetails({
 									index !== 0 ? (
 										<img
 											onClick={() => changeImage(index)}
-											src={
-												home.length !== 0 &&
-												image
-											}
+											src={home.length !== 0 && image}
 											alt=""
 											style={{
 												height: 'auto',
@@ -321,13 +312,13 @@ export default function HomeDetails({
 						<hr />
 						<br />
 						<button
-							className="btn-book-viewing" 
+							className="btn-book-viewing"
 							type="submit"
 							onClick={showBookViewing}
 						>
 							<BsEnvelope style={{ fill: 'white', marginRight: 10 }} />
 							Book Viewing
-						</button> 
+						</button>
 						<div style={{ textAlign: 'center', marginBottom: 20 }}>
 							<div className="shortlist-btn">
 								{shortlist.includes(home._id) ? (
